48a838d8d171e310f68dff08d8c64060f54726a7
Asa Shalom <asa@Asas-iMac.lan>
2020-04-05 16:51:02 -0600

    started schema and played with graphiql

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
@@ -16,8 +17,31 @@ app.use(bodyParser.json());
 // below the one endpoint for graphql typically named /graphql but could 
 // be called /api or what ever
 app.use('/graphql', graphqlHttp({
-    schema: null,
-    rootValue: {}
-}));
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    }, 
+    graphiql: true
+})
+);
 
 app.listen(1337);
